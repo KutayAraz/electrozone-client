b0fe3e5c0ac0cdbca40691e53d6edd34e2fa362f
Kutay Araz <kutayaraz@gmail.com>
2023-06-15 18:30:33 +0300

    setup eslint, eslint-airbnb-rules and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
+/* eslint-disable @typescript-eslint/quotes */
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    quotes: 0,
+  },
+};
