193397a8ba6f9f3e3d3697b2c2e10f2eccb54d23
Kutay Araz <kutayaraz@gmail.com>
2024-12-26 19:42:29 +0300

    feat: add prop type and memoization to basket.tsx

--- src/components/ui/basket/basket.tsx
+++ src/components/ui/basket/basket.tsx
@@ -1,12 +1,21 @@
+import { memo } from "react";
+
 import { ReactComponent as BasketSVG } from "@assets/svgs/basket.svg";
 
-export const Basket = ({ itemCount }: any) => {
+interface BasketProps {
+  itemCount: number;
+}
+
+export const Basket = memo(({ itemCount = 0 }: BasketProps) => {
   return (
     <div className="relative inline-block">
       <BasketSVG className="size-9" />
       {itemCount > 0 && (
-        <span className="absolute right-0 top-0 -mr-1 -mt-1 flex size-3">
-          <span className="absolute inline-flex size-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+        <span
+          className="absolute right-0 top-0 -mr-1 -mt-1 flex size-3"
+          aria-label={`${itemCount} items in basket`}
+        >
+          <span className="absolute inline-flex size-full animate-ping rounded-full bg-red-400 opacity-75" />
           <span className="relative inline-flex size-3 items-center justify-center rounded-full bg-red-500 text-xs text-white">
             {itemCount}
           </span>
@@ -14,4 +23,6 @@ export const Basket = ({ itemCount }: any) => {
       )}
     </div>
   );
-};
+});
+
+Basket.displayName = "Basket";
