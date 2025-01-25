4ee8e91543fa0716703aec289dbb2484cd2232b8
Kutay Araz <kutayaraz@gmail.com>
2025-01-25 23:47:38 +0300

    feat: create auth folder in /features
    Create auth-form to wrap sign-in/up forms, create auth-api with rtk
    query

---
+++ src/features/auth/api/auth-api.ts
@@ -0,0 +1,51 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { SignInFormInputs, SignUpFormInputs } from "../types/form-inputs";
+import { AuthResponse, RefreshTokenResponse } from "../types/responses";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    signIn: builder.mutation<AuthResponse, SignInFormInputs>({
+      query: (credentials) => ({
+        url: "/auth/signin",
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+    refreshToken: builder.query<RefreshTokenResponse, void>({
+      query: () => ({
+        url: "/auth/refresh-token",
+        method: "POST",
+      }),
+    }),
+    signUp: builder.mutation<void, SignUpFormInputs>({
+      query: (userData) => ({
+        url: "/auth/signup",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    getWishlist: builder.query<number[], void>({
+      query: () => ({
+        url: "/user/wishlist",
+        method: "GET",
+      }),
+      transformResponse: (response: any[]) => response.map((product) => product.id),
+    }),
+    mergeCarts: builder.mutation<void, { productId: number; quantity: number }[]>({
+      query: (products) => ({
+        url: "/carts/merge-carts",
+        method: "PATCH",
+        body: products,
+      }),
+    }),
+  }),
+});
+
+export const { useSignInMutation, useSignUpMutation, useGetWishlistQuery, useMergeCartsMutation } =
+  authApi;
---
+++ src/features/auth/components/auth-form.tsx
@@ -0,0 +1,47 @@
+import { CircularProgress } from "@mui/material";
+
+type AuthFormProps = {
+  onSubmit: (e: React.FormEvent) => void;
+  isValid: boolean;
+  isLoading: boolean;
+  submitText: string;
+  loadingText: string;
+  children: React.ReactNode;
+};
+
+export const AuthForm = ({
+  onSubmit,
+  isValid,
+  isLoading,
+  submitText,
+  loadingText,
+  children,
+}: AuthFormProps) => {
+  return (
+    <form action="POST" onSubmit={onSubmit}>
+      <div className="mx-2 mb-4 flex max-w-md flex-col rounded-xl bg-white p-6 shadow-md xs:mx-auto">
+        {children}
+        <button
+          type="submit"
+          className={`mt-4 w-full rounded-lg py-2 font-semibold text-white ${
+            isValid ? "bg-theme-blue hover:bg-blue-700" : "bg-gray-400"
+          } transition duration-300 ease-in-out`}
+          disabled={!isValid || isLoading}
+        >
+          {isLoading ? (
+            <div className="flex justify-center">
+              <CircularProgress
+                style={{ width: "1.25rem", height: "1.25rem" }}
+                sx={{ color: "#13193F" }}
+                className="mr-2"
+              />
+              <p>{loadingText}</p>
+            </div>
+          ) : (
+            submitText
+          )}
+        </button>
+      </div>
+    </form>
+  );
+};
