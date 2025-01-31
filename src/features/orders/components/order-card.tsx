9fb7864b58e6b132b36794778a09590f77f20f8f
Kutay Araz <kutayaraz@gmail.com>
2025-01-31 21:51:35 +0300

    feat: create features/order folder, add components
    Move order-card components from /pages subfolders to /features/order

---
+++ src/features/orders/components/order-card.tsx
@@ -0,0 +1,66 @@
+import { Divider } from "@mui/material";
+import { forwardRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { formatDateTime } from "@/utils/format-time";
+
+import { OrderCardProps, Product } from "../types";
+
+import { OrderItemCard } from "./order-item-card";
+
+export const OrderCard = forwardRef(
+  (
+    { orderId, orderTotal, orderQuantity, orderDate, user, orderItems }: OrderCardProps,
+    ref: any,
+  ) => {
+    const navigate = useNavigate();
+    const date = formatDateTime(orderDate);
+    return (
+      <div
+        key={orderId}
+        className="relative my-2 rounded-lg border border-gray-300 shadow-md transition-shadow duration-300 hover:shadow-lg"
+        ref={ref}
+      >
+        <div className="bg-gray-100 p-3">
+          <p className="font-semibold">
+            Order id: <span>{orderId}</span>
+          </p>
+          <p className="font-semibold">
+            Order total: $<span>{orderTotal}</span>
+          </p>
+          <p className="font-semibold">
+            Order quantity: <span>{orderQuantity}</span>
+          </p>
+          <p className="font-semibold">
+            Date of order: <span>{date}</span>
+          </p>
+          <p>
+            {user.firstName} {user.lastName}
+          </p>
+        </div>
+
+        <Divider />
+        <div className="noScrollbar my-2 flex space-x-2 overflow-x-auto scroll-smooth whitespace-nowrap pl-2">
+          {orderItems.map((product: Product) => (
+            <OrderItemCard
+              key={product.productId}
+              id={product.productId}
+              productName={product.productName}
+              thumbnail={product.thumbnail}
+              subcategory={product.subcategory}
+              category={product.category}
+            />
+          ))}
+        </div>
+        <button
+          onClick={() => navigate(`${orderId}`)}
+          className="absolute right-2 top-2 rounded-md border-1 bg-theme-blue p-[4px] text-white transition-colors duration-200 hover:bg-blue-800"
+        >
+          Order Details
+        </button>
+      </div>
+    );
+  },
+);
+
+OrderCard.displayName = "OrderCard";
---
+++ src/features/orders/components/order-details/order-item-card.tsx
@@ -0,0 +1,36 @@
+import { Divider } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { createUrlSlug } from "@/utils/create-url-slug";
+
+import { OrderDetailsItemCardProps } from "../../types";
+
+export const OrderItemCard = ({
+  id,
+  productName,
+  thumbnail,
+  quantity,
+  price,
+  brand,
+  subcategory,
+  category,
+}: OrderDetailsItemCardProps) => {
+  return (
+    <Link
+      to={`/category/${category}/${subcategory}/${createUrlSlug(productName)}-p-${id}`}
+      key={id}
+      className="group rounded-md border px-4 py-2 hover:border-gray-400 hover:shadow-md"
+    >
+      <div className="mb-4 flex items-center justify-center">
+        <img src={thumbnail} alt={productName} className="h-20 w-auto" />
+      </div>
+      <Divider />
+      <p className="mt-2 group-hover:text-theme-blue">{productName}</p>
+      <p className="group-hover:text-theme-blue">{brand}</p>
+      <p className="group-hover:text-theme-blue">Quantity: {quantity}</p>
+      <p className="group-hover:text-theme-blue ">Total: ${price.toFixed(2)}</p>
+    </Link>
+  );
+};
+
+export default OrderItemCard;
