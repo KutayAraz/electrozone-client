224112c6020bd575588722622eba6ac55de10b64
Kutay Araz <kutayaraz@gmail.com>
2025-03-27 22:41:23 +0300

    feat: remove cartItemCount from user-slice
    Manage count with rtk query cache

--- src/stores/slices/user-slice.ts
+++ src/stores/slices/user-slice.ts
@@ -9,7 +9,6 @@ interface User {
   city: string | null;
   isAuthenticated: boolean;
   userIntent: CheckoutIntent;
-  cartItemCount: number;
 }
 
 const initialState: User = {
@@ -17,14 +16,12 @@ const initialState: User = {
   city: null,
   isAuthenticated: false,
   userIntent: CheckoutIntent.NORMAL,
-  cartItemCount: 0,
 };
 
 interface Credentials {
   firstName: string;
   city: string;
   email: string;
-  cartItemCount: number;
 }
 
 export const userSlice = createSlice({
@@ -32,11 +29,10 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     setCredentials(state, action: PayloadAction<Credentials>) {
-      const { firstName, city, cartItemCount } = action.payload;
+      const { firstName, city } = action.payload;
       state.firstName = firstName;
       state.city = city;
       state.isAuthenticated = true;
-      state.cartItemCount = cartItemCount;
     },
     clearCredentials: () => initialState,
     updateUserInfo(state, action: PayloadAction<{ city: string }>) {
@@ -48,19 +44,11 @@ export const userSlice = createSlice({
     setGuestLocation(state, action) {
       state.city = action.payload.city;
     },
-    updateCartItemCount(state, action) {
-      state.cartItemCount = action.payload.cartItemCount;
-    },
   },
 });
 
-export const {
-  setCredentials,
-  clearCredentials,
-  updateUserInfo,
-  setUserIntent,
-  updateCartItemCount,
-} = userSlice.actions;
+export const { setCredentials, clearCredentials, updateUserInfo, setUserIntent } =
+  userSlice.actions;
 
 export const selectUser = (state: RootState) => state.user;
 export const selectIsAuthenticated = (state: RootState) => state.user.isAuthenticated;
