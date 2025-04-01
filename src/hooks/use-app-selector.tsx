7b6744e8f571d26e798d2f28b65083cd439a2483
Kutay Araz <kutayaraz@gmail.com>
2025-04-01 22:58:42 +0300

    fix: import TypedUseSelectorHook as a type

--- src/hooks/use-app-selector.tsx
+++ src/hooks/use-app-selector.tsx
@@ -1,4 +1,5 @@
-import { RootState } from "@reduxjs/toolkit/query";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { RootState } from "@/stores/store";
+import type { TypedUseSelectorHook } from "react-redux";
+import { useSelector } from "react-redux";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
