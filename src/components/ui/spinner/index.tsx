7eb6f2204fadf13cc572531e3ea70ba0ad64b3b6
Kutay Araz <kutayaraz@gmail.com>
2025-04-08 21:06:26 +0300

    fix: remove fixed css class from spinner component

--- src/components/ui/spinner/index.tsx
+++ src/components/ui/spinner/index.tsx
@@ -9,7 +9,7 @@ interface SpinnerProps {
 export const Spinner = ({ fullScreen = true, size = 40, color = "primary" }: SpinnerProps) => {
   if (fullScreen) {
     return (
-      <div className="fixed inset-0 flex items-center justify-center">
+      <div className="inset-0 flex items-center justify-center">
         <CircularProgress size={size} color={color} />
       </div>
     );
