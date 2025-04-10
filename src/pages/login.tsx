e7780099f7b1ca462bededec5aa149d95a2bfbcc
Kutay Araz <kutayaraz@gmail.com>
2025-04-10 23:10:06 +0300

    feat: use login and register hooks in /pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,61 +1,16 @@
-import { useDispatch } from "react-redux";
-
-import { useLoginMutation } from "@/features/auth/api/login";
 import { LoginForm } from "@/features/auth/components/login-form";
 import { RedirectAuthenticated } from "@/features/auth/components/redirect-authenticated";
-import { useFormError } from "@/features/auth/hooks/use-form-error";
-import { LoginSchema } from "@/features/auth/schemas/login-schema";
-import { setCredentials } from "@/stores/slices/user-slice";
-import { HttpStatus, isApiError } from "@/types/api";
+import { useLogin } from "@/features/auth/hooks/use-login";
 
 export const LoginPage = () => {
-  const dispatch = useDispatch<any>();
-
-  const { serverError, setServerError, clearServerError } = useFormError();
-  const [login, { isLoading: isLoginLoading }] = useLoginMutation();
-
-  const handleLoginSubmit = async (data: LoginSchema) => {
-    try {
-      const response = await login(data).unwrap();
-
-      // Update user state with response data
-      dispatch(
-        setCredentials({
-          firstName: response.firstName,
-          city: response.city,
-          email: response.email,
-          cartItemCount: response.cartItemCount || 0,
-        }),
-      );
-    } catch (error) {
-      if (isApiError(error)) {
-        // Handle specific error cases based on backend error types
-        switch (error.status) {
-          case HttpStatus.NOT_FOUND:
-            setServerError({
-              field: "email",
-              message: "No account found with this email",
-            });
-            break;
-          case HttpStatus.UNAUTHORIZED:
-            setServerError({
-              field: "password",
-              message: "Invalid password",
-            });
-            break;
-          default:
-            return;
-        }
-      }
-    }
-  };
+  const { submitLogin, isLoading, serverError, clearServerError } = useLogin();
 
   return (
     <RedirectAuthenticated>
-      <div className="container mx-auto px-4 py-8">
+      <div className="max-w-md flex flex-col mx-auto px-4 py-8">
         <LoginForm
-          isLoading={isLoginLoading}
-          onSubmit={handleLoginSubmit}
+          isLoading={isLoading}
+          onSubmit={submitLogin}
           serverError={serverError}
           onFieldChange={clearServerError}
         />
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,62 +1,13 @@
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
-import {
-  displayNotification,
-  NotificationType,
-} from "@/components/ui/notifications/notification-slice";
-import { paths } from "@/config/paths";
-import { useRegisterMutation } from "@/features/auth/api/register";
 import { RegisterForm } from "@/features/auth/components/register-form";
-import { useFormError } from "@/features/auth/hooks/use-form-error";
-import { RegisterSchema } from "@/features/auth/schemas/register-schema";
-import { isApiError } from "@/types/api";
+import { useRegister } from "@/features/auth/hooks/use-register";
 
 export const RegisterPage = () => {
-  const dispatch = useDispatch<any>();
-  const navigate = useNavigate();
-  const { serverError, setServerError, clearServerError } = useFormError();
-  const [register, { isLoading }] = useRegisterMutation();
-
-  const handleRegisterSubmit = async (data: RegisterSchema) => {
-    try {
-      await register(data).unwrap();
-
-      dispatch(
-        displayNotification({
-          type: NotificationType.SUCCESS,
-          message: "You have registered successfully. You can now login!",
-          autoHide: true,
-          duration: 5000,
-        }),
-      );
-      navigate(paths.auth.login.getHref());
-    } catch (error) {
-      // Using type guard to ensure type safety
-      if (isApiError(error)) {
-        if (error.status === 409) {
-          setServerError({
-            field: "email",
-            message: "This email is already taken",
-          });
-        }
-
-        dispatch(
-          displayNotification({
-            type: NotificationType.ERROR,
-            message: error.data.message,
-            autoHide: true,
-            duration: 5000,
-          }),
-        );
-      }
-    }
-  };
+  const { submitRegister, serverError, clearServerError, isLoading } = useRegister();
 
   return (
     <div className="container mx-auto px-4 py-8">
       <RegisterForm
-        onSubmit={handleRegisterSubmit}
+        onSubmit={submitRegister}
         isLoading={isLoading}
         serverError={serverError}
         onFieldChange={clearServerError}
