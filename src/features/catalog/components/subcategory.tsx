944d9adde632465d2f58c05b3a32069ba2292e19
Kutay Araz <kutayaraz@gmail.com>
2025-06-05 16:15:42 +0300

    fix: use CarouselCardProps for SubcategoryProps

--- src/features/catalog/components/subcategory.tsx
+++ src/features/catalog/components/subcategory.tsx
@@ -1,22 +1,16 @@
 import { Link } from "react-router-dom";
 
 import { Carousel } from "@/components/ui/carousel";
-import { ProductCardProps } from "@/components/ui/product-card";
+import { CarouselCardProps } from "@/components/ui/carousel/carousel-card";
 import { formatString } from "@/utils/format-casing";
 
 type ProductSectionProps = {
   title: string;
   subcategory: string;
-  products: ProductCardProps[];
-  onWishlistToggle: (productId: number) => void;
+  products: CarouselCardProps[];
 };
 
-const ProductSection = ({
-  title,
-  subcategory,
-  products,
-  onWishlistToggle,
-}: ProductSectionProps) => (
+const ProductSection = ({ title, subcategory, products }: ProductSectionProps) => (
   <>
     <h3 className="mb-3 text-center text-lg font-semibold">
       {title}{" "}
@@ -24,36 +18,29 @@ const ProductSection = ({
         {formatString(subcategory, "_")}
       </Link>
     </h3>
-    <Carousel products={products} className="mb-5" onWishlistToggle={onWishlistToggle} />
+    <Carousel products={products} className="mb-5" />
   </>
 );
 
 export type SubcategoryProps = {
   id: number;
   subcategory: string;
-  topSelling: { products: ProductCardProps[]; productQuantity: number };
-  topWishlisted: { products: ProductCardProps[]; productQuantity: number };
+  topSelling: { products: CarouselCardProps[]; productQuantity: number };
+  topWishlisted: { products: CarouselCardProps[]; productQuantity: number };
   onWishlistToggle: (productId: number) => void;
 };
 
-export const Subcategory = ({
-  subcategory,
-  topSelling,
-  topWishlisted,
-  onWishlistToggle,
-}: SubcategoryProps) => (
+export const Subcategory = ({ subcategory, topSelling, topWishlisted }: SubcategoryProps) => (
   <div className="px-6 md:px-8 xl:px-0">
     <ProductSection
       title="Top Selling Products in"
       subcategory={subcategory}
       products={topSelling.products}
-      onWishlistToggle={onWishlistToggle}
     />
     <ProductSection
       title="Top Wishlisted Products in"
       subcategory={subcategory}
       products={topWishlisted.products}
-      onWishlistToggle={onWishlistToggle}
     />
   </div>
 );
